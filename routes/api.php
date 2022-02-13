05491ff8bf533268f68f592c218d9f10aeda0a51
Frederick Adebayo <sprintcorp7@gmail.com>
2022-02-13 14:30:42 +0100

    user authentication

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
+    'prefix' => 'auth',
+], function () {
+    Route::post('/login', 'Api\AuthController@login');
+    Route::post('/register', 'Api\AuthController@register');
+
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::post('/logout', 'AuthController@logout');
+        Route::post('/refresh', 'AuthController@refresh');
+        Route::get('/user-profile', 'AuthController@userProfile');
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
