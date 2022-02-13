beb7d34ed9f5a275561812d094ff27485a90e2b1
Frederick Adebayo <sprintcorp7@gmail.com>
2022-02-13 13:57:08 +0100

    model relationship

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    public function publishers()
+    {
+        return $this->belongsToMany(Publisher::class);
+    }
 }
